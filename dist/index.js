5211bdb04e957f7dbabdf4a6599f2b6288326aff
Bilawal-Rizvi <shahb7404@gmail.com>
2026-09-07 23:35:31 +0500

    typescript started

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const userName = "Syeda";
+console.log(greet( userName));
+//# sourceMappingURL=index.js.map
