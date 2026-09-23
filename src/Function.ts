785340e038b88905222d13ae88bb26062b81ad6f
Bilawal-Rizvi <shahb7404@gmail.com>
2026-09-23 21:30:24 +0500

    function.ts

---
+++ src/Function.ts
@@ -0,0 +1,13 @@
+function makeChai(type: string, value: number) {
+    console.log(`Making ${type} chai with value ${value}`);
+}
+
+
+makeChai('masala', 30);
+function Chai(type: string, value: number): void {
+    console.log(`Making ${type} chai with value ${value}`);
+}
+function makingCHai(order:{type:string, value:number}):number{
+    console.log(`Making ${order.type} chai with value ${order.value}`);
+    return order.value
+}
